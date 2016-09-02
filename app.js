425aef4c88dd97b45dc4e2845f044790712f144b
Furkan Başaran <frknbasaran@gmail.com>
2016-09-02 13:48:02 +0300

    kjahsjkd

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
+// Simple HTTP server that renders barcode images using bwip-js.
+var http   = require('http');
 var bwipjs = require('bwip-js');
 
-// Optionally load some custom fonts.  Maximum 8.
-// OpenType and TrueType are supported.
+// Example of how to load a font into bwipjs. 
+//    bwipjs.loadFont(fontname, sizemult, fontdata)
+//
+// To unload a font (and free up space for another):
+//    bwipjs.unloadFont(fontname)
+//
 bwipjs.loadFont('Inconsolata', 108,
             require('fs').readFileSync('fonts/Inconsolata.otf', 'binary'));
 
-bwipjs.toBuffer({
-        bcid:        'code128',       // Barcode type
-        text:        '0123456789',    // Text to encode
-        scale:       3,               // 3x scaling factor
-        height:      10,              // Bar height, in millimeters
-        includetext: true,            // Show human-readable text
-        textxalign:  'center',        // Always good to set this
-        textfont:    'Inconsolata',   // Use your custom font
-        textsize:    13               // Font size, in points
-    }, function (err, png) {
-        if (err) {
-            // Decide how to handle the error
-            // `err` may be a string or Error object
-        } else {
-            // `png` is a Buffer
-            // png.length           : PNG file length
-            // png.readUInt32BE(16) : PNG image width
-            // png.readUInt32BE(20) : PNG image height
-        }
-    });
+http.createServer(function(req, res) {
+    // If the url does not begin /?bcid= then 404.  Otherwise, we end up
+    // returning 400 on requests like favicon.ico.
+    if (req.url.indexOf('/?bcid=') != 0) {
+        res.writeHead(404, { 'Content-Type':'text/plain' });
+        res.end('BWIPJS: Unknown request format.', 'utf8');
+    } else {
+        bwipjs(req, res);
+    }
+
+}).listen(3030);
